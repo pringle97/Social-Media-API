54752d3c1e95190ec7d369dc329eeb93eb799df9
pringle97 <malia.pringle@gmail.com>
2022-01-18 14:07:33 -0800

    starting user.js

--- models/User.js
+++ models/User.js
@@ -1,18 +1,24 @@
 const { Schema, model } = require('mongoose')
 
 const User = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    trim: true
   },
-  posts: [{
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v)
+      }, 
+  }
+  thoughts: [{
     type: Schema.Types.ObjectId,
-    ref: 'post'
+    ref: 'thought'
   }]
 }, { timestamps: true })
 
